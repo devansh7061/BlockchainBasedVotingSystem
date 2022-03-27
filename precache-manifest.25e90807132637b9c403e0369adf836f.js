54d823c502ce32ab206c63660cc93067ed963c9c
devansh7061 <devanshsaini67p@gmail.com>
2022-03-27 23:14:03 +0530

    Updates

---
+++ precache-manifest.25e90807132637b9c403e0369adf836f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7640d016c26a93e9dd2c6a89c11dc45e",
+    "url": "/BlockchainBasedVotingSystem/index.html"
+  },
+  {
+    "revision": "92bf955d4dd01fb8323d",
+    "url": "/BlockchainBasedVotingSystem/static/css/2.42daaa2d.chunk.css"
+  },
+  {
+    "revision": "07db4f7e37317d959431",
+    "url": "/BlockchainBasedVotingSystem/static/css/main.e811357b.chunk.css"
+  },
+  {
+    "revision": "92bf955d4dd01fb8323d",
+    "url": "/BlockchainBasedVotingSystem/static/js/2.9ea9c49f.chunk.js"
+  },
+  {
+    "revision": "07db4f7e37317d959431",
+    "url": "/BlockchainBasedVotingSystem/static/js/main.48142a13.chunk.js"
+  },
+  {
+    "revision": "cd638bfb9106affb5de3",
+    "url": "/BlockchainBasedVotingSystem/static/js/runtime-main.deb82adc.js"
+  }
+]);
